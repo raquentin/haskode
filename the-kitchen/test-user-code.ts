6345963970acbb5edf8ba21f34c2fac565eb2793
r4c3 <63271957+r4c3@users.noreply.github.com>
2022-09-18 17:10:58 -0400

    lots on frontend and backend, will explain on tues

---
+++ the-kitchen/test-user-code.ts
@@ -0,0 +1,19 @@
+export default function testUserCode(userLanguage: string,
+                                    questionID: number,
+                                    userCode: string,
+                                    questionName: string,
+                                    amountOfParameters: number,
+                                    tests: Array<any>): string {
+    let code: string = userCode + "/n/n"; //'code' will be the user's submitted McProblem() function + function calls that check if the user's functions provides expected outputs when given arguments defined in problem-data.json
+    code += codeSuffix(userLanguage, amountOfParameters, questionName, tests);
+    return result;
+}
+
+function codeSuffix(userLanguage: string, amountOfParameters: number, questionName: string, tests: Array<any>): void {
+    let functionCallSuffix: string;
+    switch(userLanguage) { //different languages have different 
+        case "Python":
+            
+    }
+
+}
