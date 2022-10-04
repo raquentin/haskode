0da8d159c0255283206cd6c649745cbca5ff722f
Giovanni Hsu <giovannihsu.123@gmail.com>
2022-10-04 17:29:57 -0400

    fixed import script bug

--- test-case-factory/eatcode/src/App.js
+++ test-case-factory/eatcode/src/App.js
@@ -23,24 +23,38 @@ function App() {
     });
   }
 
+
+  const loadScript = (src) =>
+  new Promise((resolve, reject) => {
+    if (document.querySelector(`script[src="${src}"]`)) return resolve()
+    const script = document.createElement('script')
+    script.src = src
+    script.onload = () => resolve()
+    script.onerror = (err) => reject(err)
+    document.body.appendChild(script)
+  })
   
   // not sure if this(useEffect) is the right way to do it, sometimes the page doesn't load
   useEffect(() => {
-    const google = window.google;
+    loadScript("https://accounts.google.com/gsi/client")
+      .then(() => {
+        //console.log("Hi:",window.google.accounts.id)
+        const google = window.google;
 
-    // Initialize google auth using our OAuth 2.0 Client ID
-    google.accounts.id.initialize({
-      client_id:
-        "33451040036-fcevscq8eogthqf0ve60bbdqrs03obpr.apps.googleusercontent.com",
-      callback: handleCredentialResponse,
-    });
+        // Initialize google auth using our OAuth 2.0 Client ID
+        google.accounts.id.initialize({
+          client_id:
+            "33451040036-fcevscq8eogthqf0ve60bbdqrs03obpr.apps.googleusercontent.com",
+          callback: handleCredentialResponse,
+        });
 
-    // Render the button using google's library
-    google.accounts.id.renderButton(
-      document.getElementById("buttonDiv"),
-      { theme: "outline", size: "large" } // customization attributes
-    );
-    google.accounts.id.prompt(); // also display the One Tap dialog
+        // Render the button using google's library
+        google.accounts.id.renderButton(
+          document.getElementById("buttonDiv"),
+          { theme: "outline", size: "large" } // customization attributes
+        );
+        google.accounts.id.prompt(); // also display the One Tap dialog
+      })//.catch(console.error)
   })
 
   return (
