894fcbea0d983348dd844a86f8183083e16f79ac
r4c3 <63271957+r4c3@users.noreply.github.com>
2022-11-15 19:25:52 -0500

    compartmentalized Button and CodeArea, fixed state refresh glitch

--- test-case-factory/eatcode/src/pages/question.js
+++ test-case-factory/eatcode/src/pages/question.js
@@ -2,10 +2,8 @@ import React from 'react'
 import { colors } from '../global/vars'
 import { useLocation } from 'react-router-dom'
 import { useState, createContext  } from 'react'
-import Axios from 'axios'
 import CodeArea from '../components/create/CodeArea'
 import View from '../components/create/View'
-import Button from '../components/common/Button'
 
 const Question = () => {
   const styles = {
@@ -29,37 +27,8 @@ const Question = () => {
       maxWidth: 'calc(50% - 4em)',
       width: '50%',
       height: '100%'
-    },
-    buttonDiv: {
-      display: "flex",
-      width: '100%',
-      gap: '2em',
-      justifyContent: 'space-between'
     }
   }
-  const [code, setCode] = useState(`def add(a, b):\n  return a + b;\n`);
-  const [result, setResult] = useState("");
-  const [finalResult, setFinalResult] = useState("Raw");
-
-  const handleOnChange = (event) => {
-    setCode(event.target.value);
-  }
-
-  const handleSubmit = () => {
-    console.log("Submitted Problem");
-    setFinalResult("Pending");
-    Axios.post("http://localhost:3002/problems", {
-      userCode: code, 
-      userLanguage: "python", 
-      questionID: 0
-    }).then((response) => {
-      console.log(response.data);
-      const finalWord = response.data.split("\n");
-      setFinalResult(finalWord[finalWord.length - 1])
-      setResult(response.data);
-    });
-  }
-  
 
   const UserContext = createContext()
   const problem = useLocation().state.problem
@@ -74,16 +43,8 @@ const Question = () => {
       <div style={styles.right}>
         <CodeArea 
           style={styles.textarea}
-          onChange={handleOnChange}
-          code={code} setCode={setCode}
+          //moved code={code} setCode={setCode} to CodeArea.js
         />
-        <div style={styles.buttonDiv}>
-          <Button onClick={handleSubmit} color={colors.accent1} text={"get cookin"} />
-          <Button onClick={handleSubmit} color={colors.hover} text={"stess ball"} />
-          <Button onClick={handleSubmit} color={colors.accent2} text={"see solution"} />
-        </div>
-        <h5>Result: {finalResult}</h5>
-        <pre>{result}</pre>
       </div>
     </div>
   )
