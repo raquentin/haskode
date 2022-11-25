769f25409375124d7515d638f7d8b5b6d022517d
Giovanni Hsu <giovannihsu.123@gmail.com>
2022-11-24 20:26:50 -0500

    finished testCaseFactory

--- test-case-factory/testCaseGenerator.sh
+++ test-case-factory/testCaseGenerator.sh
@@ -1,35 +1,50 @@
-NUMBER_TESTCASES = 10
+NUMBER_TESTCASES=10
 
-if [ $# -ne 2 ] && [ $# -ne 4 ]
+if [ $# -ne 2 ]
 then
-    echo "wrong number of arguments: $#, expected 2 or 4"
+    echo "wrong number of arguments: $#, expected 2"
     exit 1
 fi
 
-mkdir testCase
+FOLDER_NAME="testCase"
 
-COMMAND = "python3 "
+mkdir $FOLDER_NAME
+# cd testCase
 
-$COMMAND$1
+COMMAND="python3 " # for python
+# COMMAND = "./" #for c++
 
-if [ $# == 2 ]
-then
-    python3 $1 > input.txt
-    python3 $2 < input.txt > output.txt
-else
-    if [ "$2" == "c" ]
-    then
-        g++ 
-    else
-        python3 $1 > input.txt
-    fi
+# $COMMAND$1
 
-    if [ "$4" == "c" ]
-    then
-        echo fff
-    else
-        python3 $3 < input.txt > output.txt
-    fi
-fi
+for COUNT in $(seq 0 1 $(($NUMBER_TESTCASES-1)))
+do
+    mkdir "$FOLDER_NAME/$COUNT"
+    $COMMAND$1 > "$FOLDER_NAME/$COUNT/input.txt"
+    $COMMAND$2 < "$FOLDER_NAME/$COUNT/input.txt" > "$FOLDER_NAME/$COUNT/output.txt"
+done
+
+zip -r $FOLDER_NAME $FOLDER_NAME
+rm -r $FOLDER_NAME
+
+
+# if [ $# == 2 ]
+# then
+#     python3 $1 > input.txt
+#     python3 $2 < input.txt > output.txt
+# else
+#     if [ "$2" == "c" ]
+#     then
+#         g++ 
+#     else
+#         python3 $1 > input.txt
+#     fi
+
+#     if [ "$4" == "c" ]
+#     then
+#         echo fff
+#     else
+#         python3 $3 < input.txt > output.txt
+#     fi
+# fi
 # python3 sampleInputGenerator.py > input.txt
 # python3 sampleACCode.py < input.txt > output.txt
