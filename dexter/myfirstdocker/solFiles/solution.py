efe01807aa22c55025416a166f16f216e8404ff6
r4c3 <63271957+r4c3@users.noreply.github.com>
2022-11-28 12:27:04 -0500

    changed datil to hungarian

--- dexter/myfirstdocker/solFiles/solution.py
+++ dexter/myfirstdocker/solFiles/solution.py
@@ -1,1 +1,11 @@
-print("[0, 1]")
+nums = list(map(int, input().strip().split(" ")))
+target = int(input())
+
+seen = dict()
+
+for i, n in enumerate(nums):
+    if target-n in seen:
+        print([seen[target-n], i])
+        break
+    else:
+        seen[n] = i
