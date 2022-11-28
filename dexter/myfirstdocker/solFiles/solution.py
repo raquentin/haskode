5de0d5d2d3e110a7b205e8d44d9f85356e707f23
r4c3 <63271957+r4c3@users.noreply.github.com>
2022-11-27 20:25:15 -0500

    small formatting on View

--- dexter/myfirstdocker/solFiles/solution.py
+++ dexter/myfirstdocker/solFiles/solution.py
@@ -1,2 +1,11 @@
-def answer(a):
-  print("Mc" + a)
+nums = list(map(int, input().strip().split(" ")))
+target = int(input())
+
+seen = dict()
+
+for i, n in enumerate(nums):
+    if target-n in seen:
+        print([seen[target-n], i])
+        break
+    else:
+        seen[n] = i
