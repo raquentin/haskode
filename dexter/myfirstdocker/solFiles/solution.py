4da18a4451658f36918d21e0029ff69876305a05
Giovanni Hsu <giovannihsu.123@gmail.com>
2022-11-29 16:50:29 -0500

    clean up

--- dexter/myfirstdocker/solFiles/solution.py
+++ dexter/myfirstdocker/solFiles/solution.py
@@ -1,11 +1,17 @@
-#receieve test case from standard input
-input1 = input()
-input2 = input()
+def isPalindrome(x):
+    y=str(x)
+    lst=list(y)
+    m=len(lst)
+    if(x<0):
+        return 0
+    elif (x==0):
+        return 1
+    else:
+        flag=1
+        for x in range((m+1)//2):
+            if(lst[x]!=lst[m-1-x]):
+                return 0
+        if(flag>0):
+            return 1
 
-#compute solution
-def solve(num1, num2):
-  ans = num1 + num2
-  return ans
-  
-#print output
-print(solve(input1, input2))
+print(isPalindrome(int(input()))
