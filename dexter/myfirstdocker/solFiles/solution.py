fbb7bab06dd3074c25f52d388bbd81ec47959b11
r4c3 <63271957+r4c3@users.noreply.github.com>
2022-11-29 15:55:20 -0500

    merge gio into race - fix App.js

--- dexter/myfirstdocker/solFiles/solution.py
+++ dexter/myfirstdocker/solFiles/solution.py
@@ -1,11 +1,1 @@
-#receieve test case from standard input
-input1 = input()
-input2 = input()
-
-#compute solution
-def solve(num1, num2):
-  ans = num1 + num2
-  return ans
-  
-#print output
-print(solve(input1, input2))
+print(1)
