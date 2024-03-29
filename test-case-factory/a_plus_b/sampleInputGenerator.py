8fb71da896a8d0d60c8901a468f7bece413c5515
Race Williams <race@racewilliams.com>
2024-03-29 14:01:17 -0400

    delete shi

--- test-case-factory/a_plus_b/sampleInputGenerator.py
+++
@@ -1,5 +0,0 @@
-import random
-
-a = random.randint(-1000, 1000)
-b = random.randint(-1000, 1000)
-print(a, b)
--- test-case-factory/stock/stock.py
+++
@@ -1,7 +0,0 @@
-import random
-
-
-n = random.randint(1, 100000)
-print(n)
-for i in range(n):
-    print(random.randint(0, 1000000))
