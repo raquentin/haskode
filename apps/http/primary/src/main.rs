5a6f6bb4b0be18e408296cea9cbceb81524e6ca8
Race Williams <race@racewilliams.com>
2024-04-06 00:04:26 -0400

    db work

--- apps/http/primary/src/main.rs
+++ apps/http/primary/src/main.rs
@@ -8,16 +8,17 @@ use haskode_primary_http::api;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
+    let config = Config::parse();
 
     env_logger::init();
-
-    let config = Config::parse();
 
     let db = PgPoolOptions::new()
         .max_connections(10)
         .connect(&config.database_url)
         .await
         .context("failed to connect to database")?;
+
+    log::info!("successfully connected to database");
 
     //sqlx::migrate!().run(&db).await?;
 
