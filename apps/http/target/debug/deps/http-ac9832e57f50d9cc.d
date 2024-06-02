020070c83dabc677d36617000e61e1ea1da8bd03
Race Williams <race@racewilliams.com>
2024-06-02 16:45:54 -0400

    rust backend init

---
+++ apps/http/target/debug/deps/http-ac9832e57f50d9cc.d
@@ -0,0 +1,5 @@
+/home/race/repos/haskode/apps/http/target/debug/deps/libhttp-ac9832e57f50d9cc.rmeta: src/main.rs
+
+/home/race/repos/haskode/apps/http/target/debug/deps/http-ac9832e57f50d9cc.d: src/main.rs
+
+src/main.rs:
